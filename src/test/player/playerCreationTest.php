b80e9d0d3f1d6318b2bdf53ffa147593903a2431
toni.palanques <toni.palanques@avature.net>
2024-03-18 00:17:10 +0100

    Add coverage for player creation with specific hand

--- src/test/player/playerCreationTest.php
+++ src/test/player/playerCreationTest.php
@@ -2,7 +2,10 @@
 
 namespace player;
 
+use HMC\hand\internal\Dumb;
+use HMC\hand\internal\Standard;
 use HMC\player\PLUG\PlayerService;
+use HMC\hand\PLUG\HandService;
 use PHPUnit\Framework\TestCase;
 
 class playerCreationTest extends TestCase {
@@ -10,17 +13,37 @@ class playerCreationTest extends TestCase {
     const string USER_NAME = 'John Doe';
 
     private PlayerService $playerService;
+    private HandService $handService;
 
     public function __construct(string $name) {
         parent::__construct($name);
         $this->playerService = new PlayerService();
+        $this->handService = new HandService();
     }
 
-    public function testPlayerHasNameAndId(): void {
+    /**
+     * @throws \Exception
+     */
+    public function testCreateDefaultPlayer(): void {
         $sut = $this->playerService->createDefault(self::USER_ID, self::USER_NAME);
         $this->assertEquals(self::USER_ID, $sut->getId());
         $this->assertEquals(self::USER_NAME, $sut->getName());
         $this->assertEquals('default', $sut->getType());
+        $this->assertEquals(
+            $this->handService->getById(Standard::TYPE_ID)->getTypeId(),
+            $sut->getHand()->getTypeId()
+        );
+    }
+
+    public function testCreateDumbPlayer(): void {
+        $sut = $this->playerService->createDumb(self::USER_ID, self::USER_NAME);
+        $this->assertEquals(self::USER_ID, $sut->getId());
+        $this->assertEquals(self::USER_NAME, $sut->getName());
+        $this->assertEquals('default', $sut->getType());
+        $this->assertEquals(
+            $this->handService->getById(Dumb::TYPE_ID)->getTypeId(),
+            $sut->getHand()->getTypeId()
+        );
     }
 
     public function testPlayerIsSaved() {
