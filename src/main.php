ce285a5351a1dea3d4629f38ec7719ea1dc6e736
toni.palanques <toni.palanques@avature.net>
2024-03-20 00:30:05 +0100

    Paper rock scissors main feature

--- src/main.php
+++ src/main.php
@@ -6,28 +6,46 @@ use \HMC\game\PLUG\GameService;
 use \HMC\statistic\PLUG\StatisticService;
 
 class main {
-    const GAMES = 1000;
+    const int GAMES = 10000;
 
     private PlayerService $playerService;
     private GameService $gameService;
     private StatisticService $statisticService;
 
     public function __construct() {
+        $startTime = $this->start();
+        $this->buildServices();
+        $this->run();
+        $endTime = $this->end();
+        echo PHP_EOL . "Closing docked application in " . $endTime - $startTime . 'ms' . PHP_EOL;
+    }
+
+    private function start(): int {
         echo PHP_EOL . "Starting new PHP(" . phpversion() . ") docked application" . PHP_EOL . PHP_EOL;
+        return $this->getTime();
+    }
+
+    private function buildServices(): void {
         $this->playerService = new PlayerService();
         $this->statisticService = new StatisticService();
         $this->gameService = new GameService($this->statisticService);
-        $this->start();
-        echo PHP_EOL . "Closing docked application" . PHP_EOL . PHP_EOL;
     }
 
-    private function start(): void {
-        $cleverPlayer = $this->playerService->createDefault(1,'John Doe (the clever one)');
-        $dumbPlayer = $this->playerService->createDumb(2,'Richard Roe (the dumb one)');
+    private function run(): void {
+        $cleverPlayer = $this->playerService->createDefault(1, 'John Doe (the clever one)');
+        $dumbPlayer = $this->playerService->createDumb(2, 'Richard Roe (the dumb one)');
         for ($i = 1; $i <= self::GAMES; $i++) {
-            $this->gameService->create($cleverPlayer,$dumbPlayer);
+            $this->gameService->create($cleverPlayer, $dumbPlayer);
         }
-        $this->renderStatistics([$cleverPlayer,$dumbPlayer],$this->statisticService);
+        $this->renderStatistics([$cleverPlayer, $dumbPlayer], $this->statisticService);
+    }
+
+    private function end(): int {
+        return $this->getTime();
+    }
+
+    private function getTime(): int {
+        return (int) floor(microtime(true) * 1000);
     }
 
     private function renderStatistics(array $players, StatisticService $statistics) {
